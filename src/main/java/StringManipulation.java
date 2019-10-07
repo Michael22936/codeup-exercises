73e36dc95cda83580297a5c349b74e871650759a
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-10-07 10:44:57 -0500

    Maven excercise

---
+++ src/main/java/StringManipulation.java
@@ -0,0 +1,28 @@
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Scanner;
+
+public class StringManipulation {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter something: ");
+
+        String userInput = input.nextLine();
+
+        System.out.print("You Entered: ");
+
+        System.out.println(userInput);
+
+        if(StringUtils.isNumeric(userInput)){
+            System.out.println(StringUtils.isNumeric(userInput));
+        }else{
+            System.out.println(userInput + " is not a number");
+        }
+
+        System.out.println("Flipped Case: " + StringUtils.swapCase(userInput));
+
+        System.out.println("Reversed: " + StringUtils.reverse(userInput));
+
+    }
+}
