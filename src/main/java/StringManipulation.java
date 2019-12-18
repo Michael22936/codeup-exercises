acbc4971fe6b8637cf30b1d7df754214eea45abf
Michael McDonnell <Michael.McDonnell067@gmail.com>
2019-12-18 12:08:19 -0600

    added comments

--- src/main/java/StringManipulation.java
+++ src/main/java/StringManipulation.java
@@ -8,20 +8,24 @@ public class StringManipulation {
 
         System.out.println("Enter something: ");
 
+        //what the person enters into the comand line
         String userInput = input.nextLine();
 
         System.out.print("You Entered: ");
 
         System.out.println("\"" + userInput + "\"");
 
+        //see if the users input is a number or not
         if(StringUtils.isNumeric(userInput)){
             System.out.println(userInput + " This is a number");
         }else{
             System.out.println(userInput + " is not a number");
         }
 
+        //flippes the casing of the text
         System.out.println("Flipped Case: " + StringUtils.swapCase(userInput));
 
+        //reverses the order of what the user entered
         System.out.println("Reversed: " + StringUtils.reverse(userInput));
 
     }
